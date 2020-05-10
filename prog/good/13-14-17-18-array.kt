4db50b032ca9d10cac22dc1574de4eae6b27800d
Gabilcious <ios.base.sync.with.gabi@gmail.com>
2020-05-10 18:00:50 +0200

    TypeCheck: array, itarable

--- prog/good/13-14-17-18-array.kt
+++ prog/good/13-14-17-18-array.kt
@@ -3,18 +3,18 @@ fun g(x: Int) : Int {
 }
 
 fun main() : Unit {
-    val arr_2 = Array( 5, {i: Int -> i*i;} );
+    val arr_2: Array<Int> = Array( 5, {i: Int -> i*i;} );
     for (i in arr_2) {
         println (i);
     }
 
     val f: (Int) -> Int = {i: Int -> i*i*i;};
-    val arr_3 = Array(10, f);
-    for (i in 1 until 10) {
+    val arr_3: Array<Int> = Array(10, f);
+    for (i in [1 until 10]) {
         println(arr_3[i]);
     }
 
-    val arr_4 = Array(3, {x: Int -> g(x);});
+    val arr_4: Array<Int> = Array(3, {x: Int -> g(x);});
     for (i in arr_4) {
         println(i);
     }
